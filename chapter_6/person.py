1e9c8af6d255e8fe8f7f7c7bdd51f64ef7b579dc
Josh Hale <148706439+cablecommander@users.noreply.github.com>
2025-11-09 08:14:28 -0500

    Halfway through on the chapter

---
+++ chapter_6/person.py
@@ -0,0 +1,9 @@
+person_info = {
+    'first_name': 'Joe',
+    'last_name': 'Bobby',
+    'age': 55,
+    'city': 'New York'
+
+}
+
+print(person_info['first_name'],person_info['last_name'],person_info['age'],person_info['city'])
